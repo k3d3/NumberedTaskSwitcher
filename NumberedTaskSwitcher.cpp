f286b73549e785aa15879917a94d9e91b22a530a
Keith Morrow <keith@3d3.ca>
2013-09-15 17:45:23 -0400

    Added mouse centering, updated readme files and gave instructions on building code.

--- NumberedTaskSwitcher.cpp
+++ NumberedTaskSwitcher.cpp
@@ -9,25 +9,27 @@
     How to use:
       First, register the focused window to a hotkey by using Alt+Shift+Number, where the number is 0-9.
       Then, to bring that window back to the foreground, press Alt+Number, using the same number used to register.
+      If you want the mouse to move to the center of the window, press Alt+Shift+M to toggle.
 */
 
 #include <stdio.h>
 #include <Windows.h>
 
 int main() {
-    int i, slot;
+    int i, slot, mtoggle = 0;
     MSG msg = {0};
     HWND window_handles[10];
+    RECT window_rect;
 
     // Register hotkeys 0-9
     for (i = 0; i < 10; i++) {
-        if (RegisterHotKey(NULL, i, MOD_ALT | MOD_NOREPEAT, 0x30 + i) == TRUE) {
+        if (RegisterHotKey(NULL, i, MOD_ALT, 0x30 + i) == TRUE) {
             printf("Registering Alt+%d as hotkey\n", i);
         } else {
             printf("Failed to register Alt+%d as hotkey\n", i);
             return 1;
         }
-        if (RegisterHotKey(NULL, 10 + i, MOD_SHIFT | MOD_ALT | MOD_NOREPEAT, 0x30 + i) == TRUE) {
+        if (RegisterHotKey(NULL, 10 + i, MOD_SHIFT | MOD_ALT, 0x30 + i) == TRUE) {
             printf("Registering Alt+Shift+%d as hotkey\n", i);
         } else {
             printf("Failed to register Alt+Shift+%d as hotkey\n", i);
@@ -36,9 +38,18 @@ int main() {
         window_handles[i] = 0;
     }
 
+    if (RegisterHotKey(NULL, 20, MOD_SHIFT | MOD_ALT, 0x30 + 0x1D) == TRUE) {
+        printf("Registered Alt+Shift+M as mouse toggle hotkey\n");
+    }
+
     while (GetMessage(&msg, NULL, 0, 0) != 0) {
         if (msg.message == WM_HOTKEY) {
             slot = (msg.lParam >> 16) - 0x30;
+            if (slot == 0x1D) { // Mouse toggle hotkey
+                printf("Mouse centering %sabled.\n", mtoggle ? "dis" : "en");
+                mtoggle = !mtoggle;
+                continue;
+            }
             if (msg.lParam & MOD_SHIFT) { /* Shift is pressed, register this window */
                 window_handles[slot] = GetForegroundWindow();
                 printf("Slot %d: Registering window %d\n", slot, window_handles[slot]);
@@ -47,6 +58,18 @@ int main() {
                     printf("Slot %d: Cannot show - no window registered\n", slot);
                 } else {
                     SetForegroundWindow(window_handles[slot]);
+                    if (mtoggle) {
+                        ZeroMemory(&window_rect, sizeof(RECT));
+                        if (GetWindowRect(window_handles[slot], &window_rect) == FALSE) {
+                            printf("Could not get window dimensions.\n");
+                            continue;
+                        }
+                        if (SetCursorPos((window_rect.left + window_rect.right)/2,
+                                         (window_rect.top + window_rect.bottom)/2) == FALSE) {
+                            printf("Could not set cursor position.\n");
+                            continue;
+                        }
+                    }
                     printf("Slot %d: Showing window %d\n", slot, window_handles[slot]);
                 }
             }
--- README.md
+++ README.md
@@ -10,6 +10,10 @@ To assign the currently focused window to one of these slots, press `Alt+Shift+S
 ## Show
 To show a window that was assigned to a slot, press `Alt+Slot`, where Slot is the assigned number on your keyboard.
 
+## Mouse Centering
+If you want the mouse cursor to center on the window you are targeting, You can toggle this behavior by pressing Alt+Shift+M.
+To disable this functionality, press Alt+Shift+M again to disable.
+
 ## Example
 In case the above paragraphs aren't clear enough, try this example:
 - Find your browser, make sure it's the active window.
@@ -18,6 +22,28 @@ In case the above paragraphs aren't clear enough, try this example:
 - Press Alt+Shift+1
 
 Now, when you press Alt+0, the browser will become active, and when you press Alt+1, the other program will become active.
+
+## Building and Running
+There is no pre-built executable available, though this may change in the future.
+
+The code is very easy to build with MinGW (I have built with plain gcc and with Code::Blocks) or in Visual Studio.
+
+For MinGW (via gcc, assuming already installed):
+- Simply run `gcc.exe NumberedTaskSwitcher.cpp -o NumberedTaskSwitcher.exe`
+- If you don't want a console window, add "-mwindows" to the command line
+
+For MinGW (via Code::Blocks or other):
+- Open NumberedTaskSwitcher.cpp as a regular file
+- Build the file. This should output NumberedTaskSwitcher.exe in the same directory as the source
+- Run the executable
+- If you don't want a console window, go to Settings -> Compiler -> Linker settings -> Other linker options, and type "-mwindows" into the text box
+
+For Visual Studio 2012: (should work for other versions as well)
+- Create a new Win32 Console Application project
+- In the Application Wizard under Application type, "Windows application" will hide the console window whereas "Console application" will show it
+- Select the Empty project checkbox and click finish
+- Under the Project menu, select Add Existing Item (Shift+Alt+A) and open NumberedTaskSwitcher.cpp
+- Build the project (F7) and the executable should be in the project's directory.
 
 ## License
 This code is released in the public domain. For places where this is not possible, the code is released under a Creative Commons CC0 license, which is effectively public domain.
